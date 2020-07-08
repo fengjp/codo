84cd3f8438e9b6e8a7347bf2bd10c4c0e3dfe085
fengjp <366085674@qq.com>
2020-07-08 13:02:08 +0800

    123

--- src/view/it-manage/doc-manager.vue
+++ src/view/it-manage/doc-manager.vue
@@ -18,7 +18,7 @@
           >
             <div>
               <Icon type="ios-cloud-upload" size="50" style="color: #3399ff"></Icon>
-              <!--<p style="font-size: 12px">上传文档</p>-->
+              <p style="font-size: 12px">上传文档</p>
             </div>
           </Upload>
         </Col>
@@ -44,117 +44,117 @@
 </template>
 
 <script>
-  import {
-    UploadUrl,
-    getDocList,
-  } from '@/api/doc'
+import {
+  UploadUrl,
+  getDocList
+} from '@/api/doc'
 
-  export default {
-    data() {
-      return {
-        keyword: '修改',
-        UploadUrl: '',
-        data: [
-          {
-            title: '',
-            content: ''
-          },
-        ],
-        tableData: [],
-        pageTotal: 0, // 数据总数
-        pageNum: 1, // 当前页码
-        pageSize: 15, // 每页条数
-        //
-        searchKey: '',
-        searchValue: '',
-      }
-    },
-    methods: {
-      openUrl(url){
-        window.open(url)
-      },
-      brightenKeyword(val, keyword) {
-        if (keyword) {
-          const Reg = new RegExp(keyword, 'gi');
-          if (val) {
-            const res = val.replace(Reg, `<span style="color: #f7d008;">${keyword}</span>`);
-            // console.log(res);
-            return res;
-          }
-        } else {
-          return val
+export default {
+  data () {
+    return {
+      keyword: '修改',
+      UploadUrl: '',
+      data: [
+        {
+          title: '',
+          content: ''
         }
-      },
-      handleFormatError(file) {
-        this.$Notice.warning({
-          title: '文件格式不正确',
-          desc: '文件:' + file.name + ' 格式不正确，请选择：docx 等格式类型 '
-        });
-      },
-      handleSuccess(res, file) {
-        if (res.code === 0) {
-          this.$Message.success(`${res.msg}`)
-        } else {
-          this.$Message.error(`${res.msg}`)
-        }
-      },
-
-      getDocList(page, limit, key, value, dateValue) {
-        getDocList(page, limit, key, value, dateValue).then(res => {
-          if (res.data.code === 0) {
-            this.$Message.success(`${res.data.msg}`)
-            this.data = res.data.data
-            this.pageTotal = res.data.count
-          } else {
-            this.$Message.error(`${res.data.msg}`)
-          }
-        })
-      },
-
-      changePage(value) {
-        this.pageNum = value
-        this.getDocList(
-          this.pageNum,
-          this.pageSize,
-          this.searchKey,
-          this.searchValue,
-        )
-      },
-      // 每页条数
-      handlePageSize(value) {
-        this.pageSize = value
-        this.getDocList(
-          1,
-          this.pageSize,
-          this.searchKey,
-          this.searchValue,
-        )
-      },
-      handleReset(name) {
-        this.$refs[name].resetFields();
-      },
-      handleClear(e) {
-        if (e.target.value === '') this.tableData = this.value
-      },
-      handleSearch() {
-        this.getDocList(
-          this.pageNum,
-          this.pageSize,
-          this.searchKey,
-          this.searchValue,
-        )
-      }
-    },
-    watch: {
-      // value(val) {
-      //   this.handleSearch()
-      // }
-    },
-    mounted() {
-      this.UploadUrl = UploadUrl
-      this.getDocList(this.pageNum, this.pageSize)
+      ],
+      tableData: [],
+      pageTotal: 0, // 数据总数
+      pageNum: 1, // 当前页码
+      pageSize: 15, // 每页条数
+      //
+      searchKey: '',
+      searchValue: ''
     }
+  },
+  methods: {
+    openUrl (url) {
+      window.open(url)
+    },
+    brightenKeyword (val, keyword) {
+      if (keyword) {
+        const Reg = new RegExp(keyword, 'gi')
+        if (val) {
+          const res = val.replace(Reg, `<span style="color: #f7d008;">${keyword}</span>`)
+          // console.log(res);
+          return res
+        }
+      } else {
+        return val
+      }
+    },
+    handleFormatError (file) {
+      this.$Notice.warning({
+        title: '文件格式不正确',
+        desc: '文件:' + file.name + ' 格式不正确，请选择：docx 等格式类型 '
+      })
+    },
+    handleSuccess (res, file) {
+      if (res.code === 0) {
+        this.$Message.success(`${res.msg}`)
+      } else {
+        this.$Message.error(`${res.msg}`)
+      }
+    },
+
+    getDocList (page, limit, key, value, dateValue) {
+      getDocList(page, limit, key, value, dateValue).then(res => {
+        if (res.data.code === 0) {
+          this.$Message.success(`${res.data.msg}`)
+          this.data = res.data.data
+          this.pageTotal = res.data.count
+        } else {
+          this.$Message.error(`${res.data.msg}`)
+        }
+      })
+    },
+
+    changePage (value) {
+      this.pageNum = value
+      this.getDocList(
+        this.pageNum,
+        this.pageSize,
+        this.searchKey,
+        this.searchValue
+      )
+    },
+    // 每页条数
+    handlePageSize (value) {
+      this.pageSize = value
+      this.getDocList(
+        1,
+        this.pageSize,
+        this.searchKey,
+        this.searchValue
+      )
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
+    },
+    handleClear (e) {
+      if (e.target.value === '') this.tableData = this.value
+    },
+    handleSearch () {
+      this.getDocList(
+        this.pageNum,
+        this.pageSize,
+        this.searchKey,
+        this.searchValue
+      )
+    }
+  },
+  watch: {
+    // value(val) {
+    //   this.handleSearch()
+    // }
+  },
+  mounted () {
+    this.UploadUrl = UploadUrl
+    this.getDocList(this.pageNum, this.pageSize)
   }
+}
 </script>
 
 <style lang="less" scoped>
