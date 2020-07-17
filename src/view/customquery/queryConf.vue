1bb8adffb6a32c5762648d9dc335964ccc62b836
fengjp <366085674@qq.com>
2020-07-17 12:03:23 +0800

    自定义查询

--- src/view/customquery/queryConf.vue
+++ src/view/customquery/queryConf.vue
@@ -25,31 +25,22 @@
            :mask-closable=false :styles="{top: '20px'}">
       <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="100">
         <!--<alert>温馨提示：xxx</alert>-->
-        <FormItem label="标题" prop="title" style="margin-right:30px">
+        <FormItem label="标题" prop="title">
           <Input v-model="formValidate.title" :maxlength="50" placeholder='请输入标题'></Input>
         </FormItem>
-        <FormItem label="数据库源" prop="dblinkId" style="margin-right:30px">
-          <Input v-model="formValidate.dblinkId" :maxlength="50" placeholder="数据库源"></Input>
+        <FormItem label="数据库源" prop="dblinkId">
+          <!--<Input v-model="formValidate.dblinkId" :maxlength="50" placeholder="数据库源"></Input>-->
+          <Select v-model="formValidate.dblinkId" placeholder='请选择数据库源'>
+            <Option v-for="item in dbList" :value="item.id" :key="item.id">{{ item.name }}
+            </Option>
+          </Select>
         </FormItem>
-        <FormItem label="数据库名" prop="database" style="margin-right:30px">
+        <FormItem label="数据库名" prop="database">
           <Input v-model="formValidate.database" :maxlength="50" placeholder="请输入数据库名"></Input>
         </FormItem>
         <FormItem label="SQL语句" prop="sql">
           <editor v-model="formValidate.sql" @init="editorInit" :mode_type="mode_type" :read="editor.read"
                   :editorHeight=200 :key="`${_uid}`"></editor>
-        </FormItem>
-        <FormItem label="轮询频率" prop="times" style="margin-right:30px">
-          <RadioGroup v-model="formValidate.timesTy" vertical>
-            <Radio label="timesTy1">
-              <span style="margin-right: 15px;">每分钟</span>
-              <InputNumber :max="60" :min="1" v-model="formValidate.timesTy1Val" size="small"></InputNumber>
-            </Radio>
-            <Radio label="timesTy2">
-              <span style="margin-right: 15px;">每天固定时间</span>
-              <TimePicker size="small" format="HH:mm" v-model="formValidate.timesTy2Val" style="width: 100px;"
-                          placeholder="选择时分"></TimePicker>
-            </Radio>
-          </RadioGroup>
         </FormItem>
         <FormItem
           v-for="(item, index) in formValidate.colnames"
@@ -62,7 +53,7 @@
               <Input style="width: 80px" v-model="item.col" :maxlength="20" placeholder="字段名"></Input> ：
               <Input style="width: 120px" v-model="item.name" :maxlength="20" placeholder="字段名称"></Input>
             </Col>
-            <Col span="4" offset="1">
+            <Col span="4">
               <Button type="text" shape="circle" icon="md-close" @click="handleColRemove(index)"></Button>
             </Col>
           </Row>
@@ -73,6 +64,20 @@
               <Button type="dashed" long @click="handleColAdd" icon="md-add">增加列名</Button>
             </Col>
           </Row>
+        </FormItem>
+        <FormItem label="轮询频率" prop="times">
+          <RadioGroup v-model="formValidate.timesTy" vertical>
+            <Radio label="timesTy1">
+              <span style="margin-right: 15px;">每分钟</span>
+              <InputNumber :max="60" :min="1" v-if="formValidate.timesTy==='timesTy1'"
+                           v-model="formValidate.timesTy1Val" size="small"></InputNumber>
+            </Radio>
+            <Radio label="timesTy2">
+              <span style="margin-right: 15px;">每天固定时间</span>
+              <TimePicker size="small" format="HH:mm" v-if="formValidate.timesTy==='timesTy2'"
+                          v-model="formValidate.timesTy2Val" style="width: 100px;" placeholder="选择时分"></TimePicker>
+            </Radio>
+          </RadioGroup>
         </FormItem>
         <FormItem>
           <Button type="primary" @click="handleSubmitQuery('formValidate')">提交</Button>
@@ -88,7 +93,9 @@
   import {
     getQueryList,
     operationQuery,
-  } from '@/api/app'
+    patchquery,
+  } from '@/api/customquery/query'
+  import {getDBListForQry} from '@/api/cmdb2/db.js'
 
   export default {
     components: {editor},
@@ -109,7 +116,7 @@
           },
           {
             title: '数据库源',
-            key: 'dblinkId',
+            key: 'dblinkIdNa',
             align: 'center',
             minWidth: 100
           },
@@ -118,6 +125,36 @@
             key: 'database',
             align: 'center',
             minWidth: 100
+          },
+          {
+            title: '轮询频率',
+            key: 'timesTy',
+            align: 'center',
+            minWidth: 100
+          },
+          {
+            title: '状态',
+            key: 'status',
+            width: 80,
+            align: 'center',
+            render: (h, params, vm) => {
+              return h('div', [
+                h('i-switch', {
+                  props: {
+                    // type: "primary",
+                    value: params.row.status === '1' // 控制开关的打开或关闭状态，官网文档属性是value
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    'on-change': () => {
+                      this.onSwitch(params)
+                    }
+                  }
+                })
+              ])
+            }
           },
           {
             title: '操作', key: 'handle', width: 185, align: 'center',
@@ -162,12 +199,12 @@
         pageNum: 1, // 当前页码
         pageSize: 15, // 每页条数
         //
-        searchKey: '',
+        searchKey: 'title',
         searchValue: '',
         //
         modalMap: {
           modalVisible: false,
-          modalTitle: '添加字典',
+          modalTitle: '添加配置',
         },
         index: 0,
         formValidate: {
@@ -175,7 +212,7 @@
           title: '',
           dblinkId: '',
           database: '',
-          sql: '暂无',
+          sql: '',
           colnames: [{
             col: '',
             name: '',
@@ -183,28 +220,56 @@
           timesTy: '',
           timesTy1Val: 1,
           timesTy2Val: '',
+          status: '',
 
         },
         ruleValidate: {
           title: [{required: true, message: "标题不能为空", trigger: "blur"}],
-          dblinkId: [{required: true, message: "数据库源不能为空", trigger: "blur"}],
+          // dblinkId: [{required: true, message: "数据库源不能为空", trigger: "blur"}],
           database: [{required: true, message: "数据库名不能为空", trigger: "blur"}],
         },
+        dbList: [],
       }
     },
     methods: {
+      // 获取数据库源
+      getDBListForQry(key, value) {
+        getDBListForQry(key, value).then(res => {
+          if (res.data.code === 0) {
+            this.dbList = res.data.data
+          }
+        })
+      },
+      onSwitch(editData) {
+        const EditData = {
+          query_id: editData.row.id,
+          key: editData.column.key
+        }
+        patchquery(EditData).then(res => {
+          if (res.data.code === 0) {
+            this.$Message.success(`${res.data.msg}`)
+          } else {
+            this.$Message.error(`${res.data.msg}`)
+          }
+        })
+      },
       // 添加查询配置
       handlerQuery(paramsRow, meth, mtitle) {
         this.modalMap.modalVisible = true
         this.modalMap.modalTitle = mtitle
         this.editModalData = meth
+        this.getDBListForQry()
         if (paramsRow && paramsRow.id) {
           this.formValidate = {
             id: paramsRow.id,
             title: paramsRow.title,
             dblinkId: paramsRow.dblinkId,
             database: paramsRow.database,
+            sql: paramsRow.sql,
             colnames: paramsRow.colnames,
+            timesTy: paramsRow.timesTy,
+            timesTy1Val: parseInt(paramsRow.timesTy1Val),
+            timesTy2Val: paramsRow.timesTy2Val,
           }
         } else {
           this.formValidate = {
@@ -212,10 +277,14 @@
             title: '',
             dblinkId: '',
             database: '',
+            sql: '',
             colnames: [{
               col: '',
               name: '',
             }],
+            timesTy: 'timesTy1',
+            timesTy1Val: 1,
+            timesTy2Val: '',
           }
         }
       },
@@ -233,6 +302,7 @@
         this.$refs[value].validate((valid) => {
           if (valid) {
             setTimeout(() => {
+              console.log(this.formValidate)
               operationQuery(this.formValidate, this.editModalData).then(
                 res => {
                   if (res.data.code === 0) {
@@ -244,7 +314,7 @@
                   }
                 }
               );
-            }, 1000);
+            }, 500);
           } else {
             this.$Message.error('表单校验错误');
           }
@@ -264,15 +334,15 @@
         }
       },
       getQueryList(page, limit, key, value, dateValue) {
-        // getQueryList(page, limit, key, value, dateValue).then(res => {
-        //   if (res.data.code === 0) {
-        //     this.$Message.success(`${res.data.msg}`)
-        //     this.pageTotal = res.data.count
-        //     this.tableData = res.data.data
-        //   } else {
-        //     this.$Message.error(`${res.data.msg}`)
-        //   }
-        // })
+        getQueryList(page, limit, key, value, dateValue).then(res => {
+          if (res.data.code === 0) {
+            this.$Message.success(`${res.data.msg}`)
+            this.pageTotal = res.data.count
+            this.tableData = res.data.data
+          } else {
+            this.$Message.error(`${res.data.msg}`)
+          }
+        })
       },
       changeDate(value) {
         this.dateValue = value
--- src/view/customquery/queryShow.vue
+++ src/view/customquery/queryShow.vue
@@ -1,274 +1,81 @@
 <template>
   <div>
-    <Card>
-      <div class="search-con search-con-top">
-        <Select v-model="searchKey" class="search-col">
-          <Option v-for="item in columns" v-if="item.key !== 'handle'" :value="item.key"
-                  :key="`search-col-${item.key}`">
-            {{ item.title }}
-          </Option>
-        </Select>
-        <Input @on-change="handleClear" clearable placeholder="输入关键字搜索" class="search-input" v-model="searchValue"/>
-        <Button @click="handleSearch" class="search-btn" type="primary">搜索</Button>
-        <Button type="info" class="search-btn" @click="handlerDict('','post', '添加字典')">添加字典</Button>
-      </div>
-      <Table size="small" :columns="columns" :data="tableData"></Table>
-      <div style="margin: 10px;overflow: hidden">
-        <div style="float: left;">
-          <Page :total="pageTotal" :current="pageNum" :page-size="pageSize" :page-size-opts=[10,15,25,35,50,100]
-                show-sizer show-total @on-change="changePage" @on-page-size-change="handlePageSize"></Page>
-        </div>
-      </div>
-    </Card>
-
-    <Modal v-model="modalMap.modalVisible" :title="modalMap.modalTitle" :loading=true :footer-hide=true width="540"
-           :mask-closable=false>
-      <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="100">
-        <!--<alert>温馨提示：xxx</alert>-->
-        <FormItem label="字典名称" prop="dictname" style="margin-right:30px">
-          <Input v-model="formValidate.dictname" :maxlength="50" placeholder='请输入字典名称'></Input>
-        </FormItem>
-        <FormItem label="字典KEY" prop="dictkey" style="margin-right:30px">
-          <Input v-model="formValidate.dictkey" :maxlength="50" placeholder="请输入字典KEY"></Input>
-        </FormItem>
-        <FormItem label="字典值" prop="dictvalue" style="margin-right:30px">
-          <i-input type="textarea" v-model="formValidate.dictvalue" placeholder="[{}]"
-                   :rows="5">
-          </i-input>
-        </FormItem>
-        <FormItem>
-          <Button type="primary" @click="handleSubmitDict('formValidate')">提交</Button>
-          <Button @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
-        </FormItem>
-      </Form>
-    </Modal>
+    <Col span="11" style="margin: 10px;" v-for="(item, index) in queryObjList">
+      <Card :bordered="false">
+        <p slot="title" v-text="item.title"></p>
+        <Table :columns="item.columns" :data="item.tableData"></Table>
+      </Card>
+    </Col>
   </div>
 </template>
 
+
 <script>
   import {
-    getDictList,
-    operationDict,
-  } from '@/api/app'
+    getQueryListForshow,
+    do_sql
+  } from '@/api/customquery/query'
 
   export default {
     data() {
       return {
-        columns: [
-          {
-            title: '字典名称',
-            key: 'dictname',
-            align: 'center',
-            minWidth: 120,
-          },
-          {
-            title: '字典key',
-            key: 'dictkey',
-            align: 'center',
-            minWidth: 100
-          },
-          {
-            title: 'json数据',
-            key: 'dictvalue',
-            align: 'center',
-            minWidth: 220
-          },
-          {
-            title: '操作', key: 'handle', width: 185, align: 'center',
-            render: (h, params) => {
-              return h('div', [
-                h('Button',
-                  {
-                    props: {
-                      type: 'info',
-                      size: 'small'
-                    },
-                    style: {
-                      marginRight: '2px'
-                    },
-                    on: {
-                      click: () => {
-                        this.handlerDict(params.row, 'put', '编辑')
-                      }
-                    }
-                  }, '编辑'
-                ),
-                h(
-                  'Button',
-                  {
-                    props: {
-                      type: 'error',
-                      size: 'small'
-                    },
-                    on: {
-                      click: () => {
-                        this.handlerDeleteDict(params)
-                      }
-                    }
-                  }, '删除'
-                )
-              ])
-            }
-          }
-        ],
-        tableData: [],
         pageTotal: 0, // 数据总数
         pageNum: 1, // 当前页码
         pageSize: 15, // 每页条数
-        //
-        searchKey: 'dictkey',
-        searchValue: '',
-        //
-        modalMap: {
-          modalVisible: false,
-          modalTitle: '添加字典',
-        },
-        formValidate: {
-          id: null,
-          dictname: '',
-          dictkey: '',
-          dictvalue: '',
-        },
-        ruleValidate: {
-          dictname: [{required: true, message: "字典名称不能为空", trigger: "blur"}],
-          dictkey: [{required: true, message: "字典KEY不能为空", trigger: "blur"}],
-        },
+        queryObjList: [],
       }
     },
     methods: {
-      // 添加字典
-      handlerDict(paramsRow, meth, mtitle) {
-        this.modalMap.modalVisible = true
-        this.modalMap.modalTitle = mtitle
-        this.editModalData = meth
-        if (paramsRow && paramsRow.id) {
-          this.formValidate = {
-            id: paramsRow.id,
-            dictname: paramsRow.dictname,
-            dictkey: paramsRow.dictkey,
-            dictvalue: paramsRow.dictvalue,
-          }
-        } else {
-          this.formValidate = {
-            id: null,
-            dictname: '',
-            dictkey: '',
-            dictvalue: '',
-          }
-        }
-      },
-      handleSubmitDict(value) {
-        this.$refs[value].validate((valid) => {
-          if (valid) {
-            setTimeout(() => {
-              operationDict(this.formValidate, this.editModalData).then(
-                res => {
-                  if (res.data.code === 0) {
-                    this.$Message.success(`${res.data.msg}`);
-                    this.getDictList(this.pageNum, this.pageSize, this.searchKey, this.searchVal);
-                    this.modalMap.modalVisible = false;
-                  } else {
-                    this.$Message.error(`${res.data.msg}`);
-                  }
-                }
-              );
-            }, 1000);
-          } else {
-            this.$Message.error('表单校验错误');
-          }
-        })
-      },
-      handlerDeleteDict(params) {
-        if (confirm('确定要删除吗')) {
-          operationDict({id: params.row.id}, 'delete').then(
-            res => {
-              if (res.data.code === 0) {
-                this.$Message.success(`${res.data.msg}`);
-                this.getDictList(this.pageNum, this.pageSize, this.searchKey, this.searchVal);
-              } else {
-                this.$Message.error(`${res.data.msg}`)
-              }
-            })
-        }
-      },
-      getDictList(page, limit, key, value, dateValue) {
-        getDictList(page, limit, key, value, dateValue).then(res => {
+      getQueryListForshow(page, limit, key, value, dateValue) {
+        getQueryListForshow(page, limit, key, value, dateValue).then(res => {
           if (res.data.code === 0) {
             this.$Message.success(`${res.data.msg}`)
             this.pageTotal = res.data.count
-            this.tableData = res.data.data
+            let data = res.data.data
+            let queryObjList = res.data.data
+            for (let i in data) {
+              let item = data[i]
+              let columns = []
+              for (let j in item.colnames) {
+                let col = {title: '', key: '', align: 'center', minWidth: 80}
+                col.title = item.colnames[j].name
+                col.key = item.colnames[j].col
+                columns.push(col)
+              }
+              queryObjList[i].columns = columns
+              queryObjList[i].tableData = []
+            }
+            this.queryObjList = queryObjList
+            // console.log(this.queryObjList)
           } else {
             this.$Message.error(`${res.data.msg}`)
           }
         })
       },
-      changeDate(value) {
-        this.dateValue = value
-      },
-      changePage(value) {
-        this.pageNum = value
-        this.getDictList(
-          this.pageNum,
-          this.pageSize,
-          this.searchKey,
-          this.searchValue,
-        )
-      },
-      // 每页条数
-      handlePageSize(value) {
-        this.pageSize = value
-        this.getDictList(
-          1,
-          this.pageSize,
-          this.searchKey,
-          this.searchValue,
-        )
-      },
-      handleReset(name) {
-        this.$refs[name].resetFields();
-      },
-      handleClear(e) {
-        if (e.target.value === '') this.tableData = this.value
-      },
-      handleSearch() {
-        this.getDictList(
-          this.pageNum,
-          this.pageSize,
-          this.searchKey,
-          this.searchValue,
-        )
+      do_sql(index, query_id) {
+        do_sql('id', query_id).then(
+          res => {
+            if (res.data.code === 0) {
+              // console.log(res.data.data)
+              this.queryObjList[index].tableData = [{case_creator: "超级管理员", cnt: 24}, {case_creator: "测试", cnt: 2}]
+            }
+          }
+        );
       }
+
     },
     watch: {
-      value(val) {
-        this.handleSearch()
+      queryObjList: function () {
+        for (let i in this.queryObjList) {
+          this.do_sql(i, this.queryObjList[i].id)
+        }
       }
     },
     mounted() {
-      this.getDictList(this.pageNum, this.pageSize)
+      this.getQueryListForshow(this.pageNum, 888)
     }
   }
 </script>
 
 <style lang="less" scoped>
-  .search-con {
-    padding: 10px 0;
-
-    .search {
-      &-col {
-        display: inline-block;
-        width: 200px;
-      }
-
-      &-input {
-        display: inline-block;
-        width: 200px;
-        margin-left: 2px;
-      }
-
-      &-btn {
-        margin-left: 2px;
-      }
-    }
-  }
 </style>
