1cc1adf94a67fbdb46e2f453893046d87354d502
fengjp <366085674@qq.com>
2020-11-04 10:32:59 +0800

    单独记录请求日志

--- src/api/app.js
+++ src/api/app.js
@@ -15,6 +15,21 @@ export const getLoglist = (page, limit, key, value, date = ['', '']) => {
   })
 }
 
+export const getGetLoglist = (page, limit, key, value, date = ['', '']) => {
+  return axios.request({
+    url: '/mg/v2/app/opt_log_for_get/',
+    method: 'get',
+    params: {
+      page,
+      limit,
+      key,
+      value,
+      start_date: date[0],
+      end_date: date.length > 1 ? date[1] : ''
+    }
+  })
+}
+
 // 系统配置项
 // 发布应用选择
 export const getSysconfig = (value) => {
