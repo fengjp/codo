0599526edf487fabb2c18f4dbfbcf5248462d1a4
fengjp <366085674@qq.com>
2020-11-24 10:04:09 +0800

    table修改

--- src/view/it-manage/dataSerUp.vue
+++ src/view/it-manage/dataSerUp.vue
@@ -130,193 +130,198 @@
 </template>
 
 <script>
-  import {getCertdata} from "@/api/dataSerUp";
-  import ReportInfo from '_c/report-card'
-  import {getDate} from '@/libs/tools'
+import { getCertdata } from '@/api/dataSerUp'
+import ReportInfo from '_c/report-card'
+import { getDate } from '@/libs/tools'
 
-  export default {
-    components: {
-      ReportInfo
-    },
-    data() {
-      return {
-        timer: null, // 保存定时器，用于销毁
-        dataAll: {
-          sumUpLoad: 0,
-          todayUpLoad: 0,
-          todayDownLoad: 0,
+export default {
+  components: {
+    ReportInfo
+  },
+  data () {
+    return {
+      timer: null, // 保存定时器，用于销毁
+      dataAll: {
+        sumUpLoad: 0,
+        todayUpLoad: 0,
+        todayDownLoad: 0
+      },
+      select_date: [getDate(new Date().getTime() / 1000, 'date'), getDate(new Date().getTime() / 1000, 'date')],
+      today: [getDate(new Date().getTime() / 1000, 'date'), getDate(new Date().getTime() / 1000, 'date')],
+      upLoadLogTitle: '数据上报记录',
+      upLoadLogTableData: [],
+      upLoadLogColumn: [
+        {
+          title: '时间',
+          key: 'create_time',
+          align: 'center',
+          width: 150
         },
-        select_date: [getDate(new Date().getTime() / 1000, 'date'), getDate(new Date().getTime() / 1000, 'date')],
-        today: [getDate(new Date().getTime() / 1000, 'date'), getDate(new Date().getTime() / 1000, 'date')],
-        upLoadLogTitle: '数据上报记录',
-        upLoadLogTableData: [],
-        upLoadLogColumn: [
-          {
-            title: '时间',
-            key: 'create_time',
-            align: 'center',
-          },
-          {
-            title: '主题',
-            key: 'topic',
-            align: 'center',
-          },
-          {
-            title: '总数',
-            key: 'total',
-            align: 'center',
-          },
-          {
-            title: '成功',
-            key: 'success',
-            align: 'center',
-          },
-          {
-            title: '失败',
-            key: 'failed',
-            align: 'center',
-          },
-          {
-            title: 'REMARK',
-            key: 'extra',
-            align: 'center',
-            render: (h, params, vm) => {
-              let extra = params.row.extra
-              let remark = ''
-              if (extra){
-                for (let i in extra){
-                  remark = remark + i + ':' + extra[i] + ' '
-                }
+        {
+          title: '主题',
+          key: 'topic',
+          align: 'center',
+          width: 150
+        },
+        {
+          title: '总数',
+          key: 'total',
+          align: 'center',
+          width: 90
+        },
+        {
+          title: '成功',
+          key: 'success',
+          align: 'center',
+          width: 90
+        },
+        {
+          title: '失败',
+          key: 'failed',
+          align: 'center',
+          width: 90
+        },
+        {
+          title: 'REMARK',
+          key: 'extra',
+          align: 'center',
+          render: (h, params, vm) => {
+            let extra = params.row.extra
+            let remark = ''
+            if (extra) {
+              for (let i in extra) {
+                remark = remark + i + ':' + extra[i] + ' '
               }
-              return h('div', [
-                h('span', {}, remark)
-              ])
             }
-          },
-        ],
-        downLoadLogTitle: '不合格记录',
-        downLoadLogTableData: [],
-        downLoadLogColumn: [
-          {
-            title: '时间',
-            key: 'create_time',
-            align: 'center',
-          },
-          {
-            title: '批次号',
-            key: 'cd_batch',
-            align: 'center',
-          },
-          {
-            title: '不合格原因',
-            key: 'error_msg',
-            align: 'center',
-          },
-          // {
-          //   title: '数据',
-          //   key: 'data',
-          //   align: 'center',
-          // },
-        ],
-        upLoadErrorTitle: '上报失败记录',
-        upLoadErrorTableData: [],
-        upLoadErrorColumn: [
-          {
-            title: '时间',
-            key: 'create_time',
-            align: 'center',
-            width: 150
-          },
-          {
-            title: '主题',
-            key: 'topic',
-            align: 'center',
-          },
-          {
-            title: '上报数据',
-            key: 'data',
-            align: 'center',
-          },
-        ],
-        downLoadErrorTitle: '拉取失败记录',
-        downLoadErrorTableData: [],
-        downLoadErrorColumn: [
-          {
-            title: '时间',
-            key: 'create_time',
-            align: 'center',
-            width: 150
-          },
-          {
-            title: '主题',
-            key: 'topic',
-            align: 'center',
-          },
-          {
-            title: '拉取数据',
-            key: 'data',
-            align: 'center',
-          },
-        ],
-      };
-    },
-    methods: {
-      getCertdata(key, date) {
-        getCertdata(key, date).then(res => {
-          if (res.data.code === 0) {
-            if (key === 'UpLoadLog') {
-              this.upLoadLogTableData = res.data.data
-            }
-            if (key === 'DownLoadLog') {
-              this.downLoadLogTableData = res.data.data
-            }
-            if (key === 'UpLoadError') {
-              this.upLoadErrorTableData = res.data.data
-            }
-            if (key === 'DownLoadError') {
-              this.downLoadErrorTableData = res.data.data
-            }
-            if (key === 'dataAll') {
-              this.dataAll = res.data.data[0]
-            }
+            return h('div', [
+              h('span', {}, remark)
+            ])
           }
-        })
-      },
-      handleOk(key) {
-        this.getCertdata(key, this.select_date)
-      },
-      playTimer() {
-        this.timer = setInterval(() => {
-          this.getCertdata('dataAll', this.today)
-          this.getCertdata('UpLoadLog', this.today)
-        }, 1000 * 60 * 5)
-      },
-    },
-    watch: {
-      // reportObj: function () {
-      // this.$refs.reportInfo.initChart()
-      // }
-    },
-    mounted() {
-      this.getCertdata('UpLoadLog', this.today)
-      this.getCertdata('DownLoadLog', this.today)
-      this.getCertdata('UpLoadError', this.today)
-      this.getCertdata('DownLoadError', this.today)
-      this.getCertdata('dataAll', this.today)
-      if (this.timer) {
-        clearInterval(this.timer)// 销毁定时器 建议该在组件关闭时，再执行此方法来销毁定时器，否则定时器会一直跑下去，造成内存泄漏！！！！
-      }
-      this.playTimer()// 启用定时器
-    },
-    // 组件关闭时，执行此方法来销毁定时器
-    beforeRouteLeave(to, from, next) {
-      clearInterval(this.timer)
-      next()
-    },
-    beforeDestroy() {
-      clearInterval(this.timer)
+        }
+      ],
+      downLoadLogTitle: '不合格记录',
+      downLoadLogTableData: [],
+      downLoadLogColumn: [
+        {
+          title: '时间',
+          key: 'create_time',
+          align: 'center'
+        },
+        {
+          title: '批次号',
+          key: 'cd_batch',
+          align: 'center'
+        },
+        {
+          title: '不合格原因',
+          key: 'error_msg',
+          align: 'center'
+        }
+        // {
+        //   title: '数据',
+        //   key: 'data',
+        //   align: 'center',
+        // },
+      ],
+      upLoadErrorTitle: '上报失败记录',
+      upLoadErrorTableData: [],
+      upLoadErrorColumn: [
+        {
+          title: '时间',
+          key: 'create_time',
+          align: 'center',
+          width: 150
+        },
+        {
+          title: '主题',
+          key: 'topic',
+          align: 'center'
+        },
+        {
+          title: '上报数据',
+          key: 'data',
+          align: 'center'
+        }
+      ],
+      downLoadErrorTitle: '拉取失败记录',
+      downLoadErrorTableData: [],
+      downLoadErrorColumn: [
+        {
+          title: '时间',
+          key: 'create_time',
+          align: 'center',
+          width: 150
+        },
+        {
+          title: '主题',
+          key: 'topic',
+          align: 'center'
+        },
+        {
+          title: '拉取数据',
+          key: 'data',
+          align: 'center'
+        }
+      ]
     }
-  };
+  },
+  methods: {
+    getCertdata (key, date) {
+      getCertdata(key, date).then(res => {
+        if (res.data.code === 0) {
+          if (key === 'UpLoadLog') {
+            this.upLoadLogTableData = res.data.data
+          }
+          if (key === 'DownLoadLog') {
+            this.downLoadLogTableData = res.data.data
+          }
+          if (key === 'UpLoadError') {
+            this.upLoadErrorTableData = res.data.data
+          }
+          if (key === 'DownLoadError') {
+            this.downLoadErrorTableData = res.data.data
+          }
+          if (key === 'dataAll') {
+            this.dataAll = res.data.data[0]
+          }
+        }
+      })
+    },
+    handleOk (key) {
+      this.getCertdata(key, this.select_date)
+    },
+    playTimer () {
+      this.timer = setInterval(() => {
+        this.getCertdata('dataAll', this.today)
+        this.getCertdata('UpLoadLog', this.today)
+      }, 1000 * 60 * 5)
+    }
+  },
+  watch: {
+    // reportObj: function () {
+    // this.$refs.reportInfo.initChart()
+    // }
+  },
+  mounted () {
+    this.getCertdata('UpLoadLog', this.today)
+    this.getCertdata('DownLoadLog', this.today)
+    this.getCertdata('UpLoadError', this.today)
+    this.getCertdata('DownLoadError', this.today)
+    this.getCertdata('dataAll', this.today)
+    if (this.timer) {
+      clearInterval(this.timer)// 销毁定时器 建议该在组件关闭时，再执行此方法来销毁定时器，否则定时器会一直跑下去，造成内存泄漏！！！！
+    }
+    this.playTimer()// 启用定时器
+  },
+  // 组件关闭时，执行此方法来销毁定时器
+  beforeRouteLeave (to, from, next) {
+    clearInterval(this.timer)
+    next()
+  },
+  beforeDestroy () {
+    clearInterval(this.timer)
+  }
+}
 </script>
 
 <style lang="less" scoped>
